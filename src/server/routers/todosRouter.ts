e131ba66f8e59cfe870f666ec418ddafc6b3eb7b
Eddy  Hernández <eddyhdzg@eddys-mbp.lan>
2023-08-10 19:04:51 -0500

    todos 1

--- src/server/routers/todosRouter.ts
+++ src/server/routers/todosRouter.ts
@@ -17,4 +17,38 @@ export const todosRouter = t.router({
         },
       });
     }),
+  deleteTodo: t.procedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id } = input;
+
+      return ctx.prisma.todo.delete({
+        where: {
+          id,
+        },
+      });
+    }),
+
+  getTodo: t.procedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { id } = input;
+
+      return ctx.prisma.todo.findUnique({
+        where: {
+          id,
+        },
+      });
+    }),
+  getTodos: t.procedure.query(async ({ ctx }) => {
+    return ctx.prisma.todo.findMany();
+  }),
 });
